f7010cdb2a439538ecd67e0d8ab7fb8ee9dac286
Ostap Tyvonovych <ostaptyv@gmail.com>
2023-06-21 22:05:04 +0300

    wip

--- Notes/AppDelegate.swift
+++ Notes/AppDelegate.swift
@@ -20,31 +20,31 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return true
     }
     
-    func applicationDidEnterBackground(_ application: UIApplication) {
-        
-        let navigationVC = window?.rootViewController as! UINavigationController
-        let notesListVC = navigationVC.viewControllers.first! as! NotesListViewController
-        
-        if !notesListVC.isSortNewToOld {
-            database.reverseNotesCollection()
-        }
-        /*
-         Since initial value of isSortNewToOld property in NotesListViewController is true, we should take care about to return the state of the system to appropriate this value (basically here and further we will working out case when isSortNewToOld can be "false" because when isSortNewToOld is "true" nothing bad happens).
-         In case if sort was changed to "old to new" and reversedCollection() was called, here we call reversedCollection(), in fact making it "new to old", but not changing value of isSortNewToOld (isSortNewToOld sill will be "false").
-        And there're 2 ways:
-         1) if app will be closed, then value of isSortNewToOld will be reseted and initialized with true next time app will launch, and order in list.source will correlate with isSortNewToOld.
-         Otherway, 2) if app will continue its work and won't be closed, we call reverseCollection() in applicationWillEnterForeground(_:) method to return it to state "old to new"
-         */
-    }
-    
-    func applicationWillEnterForeground(_ application: UIApplication) {
-        let navigationVC = window?.rootViewController as! UINavigationController
-        let notesListVC = navigationVC.viewControllers.first! as! NotesListViewController
-        
-        if !notesListVC.isSortNewToOld {
-            database.reverseNotesCollection()
-        }
-    }
+//    func applicationDidEnterBackground(_ application: UIApplication) {
+//        
+//        let navigationVC = window?.rootViewController as! UINavigationController
+//        let notesListVC = navigationVC.viewControllers.first! as! NotesListViewController
+//        
+//        if !notesListVC.isSortNewToOld {
+//            database.reverseNotesCollection()
+//        }
+//        /*
+//         Since initial value of isSortNewToOld property in NotesListViewController is true, we should take care about to return the state of the system to appropriate this value (basically here and further we will working out case when isSortNewToOld can be "false" because when isSortNewToOld is "true" nothing bad happens).
+//         In case if sort was changed to "old to new" and reversedCollection() was called, here we call reversedCollection(), in fact making it "new to old", but not changing value of isSortNewToOld (isSortNewToOld sill will be "false").
+//        And there're 2 ways:
+//         1) if app will be closed, then value of isSortNewToOld will be reseted and initialized with true next time app will launch, and order in list.source will correlate with isSortNewToOld.
+//         Otherway, 2) if app will continue its work and won't be closed, we call reverseCollection() in applicationWillEnterForeground(_:) method to return it to state "old to new"
+//         */
+//    }
+//    
+//    func applicationWillEnterForeground(_ application: UIApplication) {
+//        let navigationVC = window?.rootViewController as! UINavigationController
+//        let notesListVC = navigationVC.viewControllers.first! as! NotesListViewController
+//        
+//        if !notesListVC.isSortNewToOld {
+//            database.reverseNotesCollection()
+//        }
+//    }
 
 }
 
